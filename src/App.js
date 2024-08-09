b3860318073a0053f6dcfc092b5903978194522e
bourrasque <stampixel.t@gmail.com>
2024-08-09 19:33:52 -0400

    Init

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="App h-full overflow-x-hidden ">
+            <header>
+
+            </header>
+
+            <nav>
+
+            </nav>
+
+            <div id={"homePage"} className={"h-screen bg-[url('../public/illustration/frontpage-pc.png')] bg-cover bg-no-repeat bg-top "}>
+                <div className={"h-screen w-screen flex flex-row p-32"}>
+                    <div id={"left"} className={"w-1/2"}>
+                        <div className={"text-left text-white"}>
+                            <h2 className={""}>Ignition Hacks</h2>
+                            <h4>Online Hackathon / August 16-18, 202</h4>
+                            <button>REGISTER HERE</button>
+                        </div>
+
+                    </div>
+                    <div className={"w-1/2"}>
+
+                    </div>
+                </div>
+
+            </div>
+
+            <div id={"wereBack"}>
+
+            </div>
+
+            <div id={"faq"}>
+
+            </div>
+
+            <div id={"apply"}>
+
+            </div>
+
+            <div id={"joinUs"}>
+
+            </div>
+
+            <div id={"lastYear"}>
+
+            </div>
+
+            <div id={"sponsors"}>
+
+            </div>
+
+            <div id={"throwBack"}>
+
+            </div>
+
+            <div id={"aboutUs"}>
+
+            </div>
+
+            <div id={"ourTeam"}>
+
+            </div>
+
+            <footer>
+
+            </footer>
+        </div>
+    );
+}
+
+export default App;
