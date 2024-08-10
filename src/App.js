b567e1f46afe46c62dd78dfee785ad9720f8ffe8
bourrasque <stampixel.t@gmail.com>
2024-08-10 14:19:56 -0400

    Frontpage

--- src/App.js
+++ src/App.js
@@ -11,22 +11,74 @@ function App() {
 
             </nav>
 
-            <div id={"homePage"} className={"h-screen bg-[url('../public/illustration/frontpage-pc.png')] bg-cover bg-no-repeat bg-top "}>
-                <div className={"h-screen w-screen flex flex-row p-32"}>
-                    <div id={"left"} className={"w-1/2"}>
-                        <div className={"text-left text-white"}>
-                            <h2 className={""}>Ignition Hacks</h2>
-                            <h4>Online Hackathon / August 16-18, 202</h4>
-                            <button>REGISTER HERE</button>
+            <div id={"bg-1"} className={"flex flex-col"}>
+                <div id={"homePage"} className={"bg-[url('../public/illustration/frontpage-pc.png')] bg-cover bg-no-repeat bg-top "}>
+                    <div className={"h-screen w-screen flex flex-row p-32 pb-72"}>
+                        <div id={"left"} className={"w-1/2"}>
+                            <div className={"text-left text-white"}>
+                                <h2 className={""}>Ignition Hacks</h2>
+                                <h4>Online Hackathon / August 16-18, 202</h4>
+                                <button>REGISTER HERE</button>
+                            </div>
+
                         </div>
 
+                        <div className={"w-1/2"}>
+
+                        </div>
                     </div>
-                    <div className={"w-1/2"}>
+
+                    <div id={"wereBack"} className={"h-screen flex items-center justify-center"}>
+                        <div className={"bg-cover bg-no-repeat bg-top h-1/2 m-auto bg-blue-50 rounded-3xl m-52"}>
+                            <div className={""}>
+                                <div className={"h-12 rounded-t-3xl bg-indigo-400"}>
+
+                                </div>
+                                <div>
+                                    <h3>We're back!</h3>
+                                    <p>Ignition Hacks is a <span
+                                        className={"font-black"}>36-hour online hackathon</span> that
+                                        focuses on the intersection between <span className={"font-black"}>business and technology</span>.
+                                        It is a fun-filled event featuring awesome prizes, SWAG, engaging workshops, and
+                                        more!
+                                    </p>
+
+                                </div>
+
+
+                            </div>
+                            {/*<div>*/}
+                            {/*    <img src={""} alt={""}/>*/}
+                            {/*</div>*/}
+                        </div>
+
+                        {/*<div id={"wereBack"} className={"h-screen flex items-center justify-center"}>*/}
+                        {/*    <div className={"bg-[url('../public/illustration/textbox/iggywindow.png')] bg-cover bg-no-repeat bg-top h-1/2 m-auto"}>*/}
+                        {/*        <div className={"pt-32 ml-72 text-left w-1/3"}>*/}
+                    {/*            <h3>We're back!</h3>*/}
+                    {/*            <p>Ignition Hacks is a <span*/}
+                    {/*                className={"font-black"}>36-hour online hackathon</span> that*/}
+                    {/*                focuses on the intersection between <span className={"font-black"}>business and technology</span>.*/}
+                    {/*                It is a fun-filled event featuring awesome prizes, SWAG, engaging workshops, and*/}
+                    {/*                more!*/}
+                    {/*            </p>*/}
+
+                    {/*        </div>*/}
+                    {/*        /!*<div>*!/*/}
+                    {/*        /!*    <img src={""} alt={""}/>*!/*/}
+                    {/*        /!*</div>*!/*/}
+                    {/*    </div>*/}
 
                     </div>
+
+                    <div id={"faq"}>
+
+                    </div>
+
                 </div>
 
             </div>
+
 
             <div id={"wereBack"}>
 
